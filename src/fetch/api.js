7b55a153f40480c576a71c84245890d2cca44d1b
Tiago Garbi Baganha <79227612+tiagogb21@users.noreply.github.com>
2022-06-01 19:01:08 -0300

    Update api.js

--- src/fetch/api.js
+++ src/fetch/api.js
@@ -1,5 +1,3 @@
-const API_KEY = 'AIzaSyDjjYb4qrqOoFfcatHxMgQI8IUDUj8I1V8';
-
 const URL_API = (query) => `
   https://www.googleapis.com/customsearch/v1?key=${API_KEY}
   &cx=017576662512468239146:omuauf_lfve&q=${query}`;
